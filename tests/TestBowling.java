ab34e2fe5db30b7a65d7cf7a316c85b630a7f45e
somename <someemail>
2017-06-09 15:33:44 +0200

    besouro automatic message

--- tests/TestBowling.java
+++ tests/TestBowling.java
@@ -107,7 +107,7 @@ public class TestBowling {
 		game.addFrame(new Frame(10, 0));
 		game.addFrame(new Frame(5, 2));
 		
-		int score = 1;
+		int score = 22;
 		
 		assertEquals(score, game.score());
 		
